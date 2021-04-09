5791ec332fb9e6d1413d71334c1ac5379368d4b1
Enrico Olivelli <eolivelli@datastax.com>
2021-04-09 17:56:23 +0200

    more fixes

--- tck-executor/run.sh
+++ tck-executor/run.sh
@@ -8,16 +8,19 @@ PATH=$PATH:$JMS_HOME/bin:$TS_HOME/tools/ant/bin
 
 cp ts.* $TS_HOME/bin
 
-cd $TS_HOME/src/com/sun/ts/tests/jms/core20/connectionfactorytests
-#cd $TS_HOME/src/com/sun/ts/tests/jms/core20/appclient
-#cd $TS_HOME/src/com/sun/ts/tests/jms/core20/jmsconsumertests
+#cd $TS_HOME/src/com/sun/ts/tests/jms/core20
+#cd $TS_HOME/src/com/sun/ts/tests/jms/core20/connectionfactorytests
+#cd $TS_HOME/src/com/sun/ts/tests/jms/core20/runtimeexceptiontests
+
+cd $TS_HOME/src/com/sun/ts/tests/jms/core20/jmsconsumertests
 #cd $TS_HOME/src/com/sun/ts/tests/jms/core20/jmscontextqueuetests
 #cd $TS_HOME/src/com/sun/ts/tests/jms/core20/jmscontexttopictests
 #cd $TS_HOME/src/com/sun/ts/tests/jms/core20/jmsproducerqueuetests
 #cd $TS_HOME/src/com/sun/ts/tests/jms/core20/jmsproducertopictests
 #cd $TS_HOME/src/com/sun/ts/tests/jms/core20/messageproducertests
-#cd $TS_HOME/src/com/sun/ts/tests/jms/core20/runtimeexceptiontests
 #cd $TS_HOME/src/com/sun/ts/tests/jms/core20/sessiontests
+
+#cd $TS_HOME/src/com/sun/ts/tests/jms/core20/appclient
 
 ant runclient
 
