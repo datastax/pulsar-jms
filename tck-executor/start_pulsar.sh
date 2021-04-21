c92116a2de8bd2b03bd599a466a126c46a7efc59
Enrico Olivelli <eolivelli@datastax.com>
2021-04-21 18:04:14 +0200

    wait for pulsar to start

--- tck-executor/start_pulsar.sh
+++ tck-executor/start_pulsar.sh
@@ -10,7 +10,11 @@ docker rm -f pulsar-jms-runner
 docker run --name pulsar-jms-runner -v $HERE/conf:/pulsar/conf -d -p 8080:8080 -p 6650:6650 $IMAGENAME /pulsar/bin/pulsar standalone 
 # Wait for pulsar to start
 echo "Waiting 15 seconds"
-sleep 15
+
+wget http://localhost:8080/admin/v2/clusters
+
+sleep 10
+
 # This step is needed on 2.7.x in order to start the Transaction Coordinator
 #docker exec pulsar-jms-runner bin/pulsar initialize-transaction-coordinator-metadata -cs 127.0.0.1:2181 -c standalone
 
