a4d452b1133ccf8bfe4010e3b4be50af9eb07480
Enrico Olivelli <enrico.olivelli@datastax.com>
2022-10-03 11:15:24 +0200

    Clean up

--- pulsar-jms/src/test/java/com/datastax/oss/pulsar/jms/JNDITest.java
+++ pulsar-jms/src/test/java/com/datastax/oss/pulsar/jms/JNDITest.java
@@ -89,7 +89,7 @@ public class JNDITest {
         PulsarDestination queue = (PulsarDestination) jndiContext.lookup("queues/" + queueName);
         PulsarDestination topic = (PulsarDestination) jndiContext.lookup("topics/" + topicName);
 
-        Producer<byte[]> producer = factory.getProducerForDestination(queue, false);
+        Producer<byte[]> producer = factory.getProducerForDestination(queue, false, PulsarMessage.DEFAULT_PRIORITY);
         // test that configuration is fully passed
         assertEquals("the-name", producer.getProducerName());
 
