44ba969b332209a3653059dc09fecba59dad3745
Enrico Olivelli <enrico.olivelli@datastax.com>
2024-05-14 18:31:55 +0200

    JMSPublishFilters: fix metadata cache and introduce new threadpool for acks (#148)

--- pulsar-jms-filters/src/main/java/com/datastax/oss/pulsar/jms/selectors/JMSFilter.java
+++ pulsar-jms-filters/src/main/java/com/datastax/oss/pulsar/jms/selectors/JMSFilter.java
@@ -736,7 +736,7 @@ public class JMSFilter implements EntryFilter {
     // we pre-compute the type in order to avoid to scan the list to fine the type
     String type = SYSTEM_PROPERTIES_TYPES.get(name);
     if (type == null) {
-      type = propertyType(name);
+      type = cacheProperties.get(propertyType(name));
     }
     String value = cacheProperties.get(name);
     return getObjectProperty(value, type);
