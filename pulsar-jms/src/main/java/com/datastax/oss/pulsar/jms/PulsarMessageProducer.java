53d9def5e77adea902386d203d1c9cac2c42d0d5
sandeep-ctds <ext-sandeep.malla@datastax.com>
2025-03-04 18:22:29 +0530

    organized import

--- pulsar-jms/src/main/java/com/datastax/oss/pulsar/jms/PulsarMessageProducer.java
+++ pulsar-jms/src/main/java/com/datastax/oss/pulsar/jms/PulsarMessageProducer.java
@@ -44,7 +44,6 @@ import jakarta.jms.TopicPublisher;
 import java.util.ArrayList;
 import java.util.Enumeration;
 import java.util.List;
-import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.TimeUnit;
 import lombok.extern.slf4j.Slf4j;
